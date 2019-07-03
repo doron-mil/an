1332c3bc6f4699ac4179a98fffb3364301e03e6b
doron <doron.miller@gmail.com>
2019-07-04 00:09:04 +0300

    master - reduce

--- population_pyramid.js
+++ population_pyramid.js
@@ -1,58 +1,66 @@
+const createAgeLevelsArray = () => {
+    return Array(24).fill(5).map((value, index) => value * index);
+};
+
+const ageLevelsArray = createAgeLevelsArray();
+
 class Individual {
-  constructor(aAge, aIsMale) {
-    this.age = aAge;
-    this.isMale = aIsMale;
-  }
+    constructor(aAge, aIsMale) {
+        this.age = aAge;
+        this.isMale = aIsMale;
+    }
 }
 
 class PopulationPyramid {
-  constructor() {
-    this.females = new Map();
-    this.males = new Map();
+    constructor() {
+        this.females = new Map();
+        this.males = new Map();
 
-    for (let i = 0; i < 120; i += 5) {
-      this.females.set(i, 0);
-      this.males.set(i, 0);
+        ageLevelsArray.reduce((previousValue, currentValue) => {
+            this.females.set(currentValue, 0);
+            this.males.set(currentValue, 0);
+        });
     }
-  }
 }
 
 const mockPopulationData = (aPopulationSize) => {
-  const retPopulationDataArray = new Array();
-  for (let i = 0; i < aPopulationSize; i++) {
-    const age = Math.random() * 120;
-    const isMale = Math.random() > 0.5;
-    const newIndividaul = new Individual(age, isMale);
-    retPopulationDataArray.push(newIndividaul);
-  }
-  return retPopulationDataArray;
+    return Array( aPopulationSize ).fill(null).reduce(
+        (previousValue) => {
+            const age = Math.random() * 120;
+            const isMale = Math.random() > 0.5;
+            const newIndividual = new Individual(age, isMale);
+            previousValue.push(newIndividual);
+            return previousValue;
+        }, new Array());
+    const retPopulationDataArray = new Array();
 };
 
 const printPopulationPyramid = (aPopulationPyramid) => {
-  for (let i = 0; i < 120; i += 5) {
-    const femalesCount = aPopulationPyramid.females.get(i);
-    const malesCount = aPopulationPyramid.males.get(i);
-    const ident = i < 10 ? '\t\t' : '\t';
-    console.log(`For age ${i} - ${i + 5} ${ident}: F = ${femalesCount} , M = ${malesCount}`);
-  }
+    ageLevelsArray.reduce((accumulator, value) => {
+        const femalesCount = aPopulationPyramid.females.get(value);
+        const malesCount = aPopulationPyramid.males.get(value);
+        const ident = value < 10 ? '\t\t' : '\t';
+        console.log(`For age ${value} - ${value + 5} ${ident}: F = ${femalesCount} , M = ${malesCount}`);
+    });
 };
 
 const createPopulationPyramid = (aPopulationDataArray) => {
-  const newPopulationPyramid = new PopulationPyramid();
-  aPopulationDataArray.forEach( (individual) => {
-    const age = individual.age;
-    let ageLevel = Math.floor(age / 5) * 5;
-    if(ageLevel > 115 ){
-      ageLevel = 115
-    }
-    const genderMap = individual.isMale ? newPopulationPyramid.males : newPopulationPyramid.females;
-    genderMap.set(ageLevel, genderMap.get(ageLevel) + 1);
-  });
-  return newPopulationPyramid;
+    return aPopulationDataArray.reduce((accumulator, individualValue) => {
+        const age = individualValue.age;
+        let ageLevel = Math.floor(age / 5) * 5;
+        if (ageLevel > 115) {
+            ageLevel = 115;
+        }
+        const genderMap = individualValue.isMale ? accumulator.males : accumulator.females;
+        genderMap.set(ageLevel, genderMap.get(ageLevel) + 1);
+        return accumulator;
+    }, new PopulationPyramid());
 };
 
 const populationSize = 1000000;
 const mockPopulationDataArray = mockPopulationData(populationSize);
 const populationPyramid = createPopulationPyramid(mockPopulationDataArray);
 printPopulationPyramid(populationPyramid);
+
+
 
